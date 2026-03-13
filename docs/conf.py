c1f109a39efdef51587f87786bd9de5d742da93b
oisin-m <oisinmmm@gmail.com>
2026-03-13 12:56:47 +0100

    fix: update docs with custom style

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["style.css"]
+html_css_files = ["custom.css"]
 
 
 # html_logo = "https://github.com/ecmwf/logos/raw/refs/heads/main/logos/earthkit/earthkit-geo-dark.svg"
